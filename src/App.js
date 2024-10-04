d7df2d2bf1f61c23c794a6fbd37e4ead0e2dfbf3
PhatTanNguyenG00425232 <G00425232@atu.ie>
2024-10-04 10:39:48 +0000

    Exercise 3 finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Content from './components/Content';
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   
   return (
-    <div>
-        <h1>Hello World!</h1>
-        <h2>It is {new DataTransfer().toLocaleTimeString()}.</h2>    
-      
+    <div className="App">
+          
+        <Content></Content> 
+        <Header></Header>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+const Header = () => {
+    return <h1>My Header in another component</h1>
+};
+export default Header;
