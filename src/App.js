7381a3ae38670f67ae26fb650540e9ee5883e7d6
PhatTanNguyenG00425232 <G00425232@atu.ie>
2024-10-04 11:22:24 +0000

    Exercise 5 completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
+
 function App() {
   
   return (
     <Router>
       <NavigationBar />
       <Routes>
-        <Route path="/home" element={<Content />} />
-        <Route path="/read" element={<h1>Read Component</h1>} />
-        <Route path="/create" element={<h1>Create Component</h1>} />
+        <Route path="/home" element={<Content></Content> } />
+        <Route path="/read" element={<Header></Header>} />
+        <Route path="/create" element={<Footer></Footer>} />
       </Routes>
       <Footer />
     </Router>
